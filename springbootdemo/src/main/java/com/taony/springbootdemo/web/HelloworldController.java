7b9d7dbf9a4245dc04f17bde7b51c3dbb907c905
taony <taony@users.noreply.github.com>
2018-03-04 14:09:10 +0800

    using thymeleaf template

--- springbootdemo/src/main/java/com/taony/springbootdemo/web/HelloworldController.java
+++ springbootdemo/src/main/java/com/taony/springbootdemo/web/HelloworldController.java
@@ -1,17 +1,18 @@
 package com.taony.springbootdemo.web;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class HelloworldController {
 
     @RequestMapping("/hello")
-    public String index() {
-        System.out.print("Hello");
-        return "Helloworld";
+    public String index(Model model) {
+        model.addAttribute("name","taony");
+        return "hello";
     }
 
 }
